2386783caa7234d28294c55ba2fc9b03b01dbdeb
arcodez <abelileitor123@gmail.com>
2022-04-05 00:15:30 -0400

    add styles to results page

--- components/ListOfAnimals/index.jsx
+++ components/ListOfAnimals/index.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import AnimalCard from "../AnimalCard";
 import "./style.css";
 
-const ListOfAnimals = () => {
+const ListOfAnimals = ({
+  loteryName = "Lotto Activo",
+  timeLoteryValue = false,
+}) => {
   return (
     <div className="animals_cards">
-      <h1 className="animals_list_title">ListOfAnimals</h1>
+      <h1 className="animals_list_title">{loteryName}</h1>
       <div className="animals_row">
         {[1, 2, 3, 4, 5, 6, 7, 8].map((card, index) => (
-          <AnimalCard key={index} {...card} />
+          <AnimalCard key={index} {...card} timeLotery={timeLoteryValue} />
         ))}
       </div>
     </div>
