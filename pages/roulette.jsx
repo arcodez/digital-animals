94d9909a81e35bea1af41c79a22a848dc1032274
arcodez <abelileitor123@gmail.com>
2022-04-18 13:30:49 -0430

    add roulette

---
+++ pages/roulette.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RoulettePage from "../components/Pages/RoulettePage";
+
+const Roulette = () => {
+  return (
+    <>
+      <RoulettePage />
+    </>
+  );
+};
+
+export default Roulette;
